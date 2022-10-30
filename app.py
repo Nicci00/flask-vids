1db5281adbca86d69c0a0fe8a0cb4db7443bc43d
Nicci00 <armadurapersonal@gmail.com>
2022-10-30 04:36:49 -0300

    update to python 3.10 and py-av

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 #-*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
-from flask import Flask, render_template, request, redirect, session, abort
-from flask.ext.sqlalchemy import SQLAlchemy
-from werkzeug import secure_filename
-from werkzeug.contrib.fixers import ProxyFix
-
-from ConfigParser import SafeConfigParser
-
-from ffvideo import VideoStream ##thumbnails
-import magic ##filetype check
-
-import random
-import sys
-import os
-import logging
-import shutil
 import hashlib
+import logging
+import os
+import shutil
 import tempfile
+
+import magic 
+
+from configparser import ConfigParser
+
+import av
+from flask import Config, Flask, abort, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 ## Set up
 app = Flask(__name__)
 
 ProxyFix(app.wsgi_app)
 
-parser = SafeConfigParser()
+parser = ConfigParser()
 parser.read('config.ini')
 
 app.secret_key = parser.get('application', 'key')
@@ -37,220 +34,213 @@ app.config['thumbs_dir'] = parser.get('application', 'thumbs_dir')
 app.config['MAX_CONTENT_LENGTH'] = int(parser.get('application', 'max_file_size'))* 1024 * 1024
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % parser.get('database', 'db_name')
 
+APP_ROOT = os.getcwd()
+
 db = SQLAlchemy(app)
 
-logging.basicConfig(filename='videos-log.log', level=logging.ERROR)
+#logging.basicConfig(filename='videos-log.log', level=logging.ERROR)
 
 ##view funcs
 @app.route('/', methods = ['GET', 'POST'])
 def videos():
-	error = None
-	if request.method == 'POST':
-		file = request.files['file']
+    error = None
+    if request.method == 'POST':
+        file = request.files['file']
 
-		if not file:
-			return "<script> alert('No file to be uploaded'); \
-				window.location.replace('');</script>", 400
+        if not file:
+            return "<script> alert('No file to be uploaded'); \
+                window.location.replace('');</script>", 400
 
-		file.filename = secure_filename(file.filename)
-		temp_file = os.path.join(tempfile.gettempdir(), file.filename)
-		file.save(temp_file)
+        file.filename = secure_filename(file.filename)
+        temp_file = os.path.join(tempfile.gettempdir(), file.filename)
+        file.save(temp_file)
 
-		#mime check
-		mime = magic.from_file(temp_file, mime=True)
+        #mime check
+        mime = magic.from_file(temp_file, mime=True)
 
-		if mime not in app.config['allowed_mime']:
-			os.remove(temp_file)
-			return "<script> alert('File type not allowed!'); \
-				window.location.replace('');</script>", 415
+        if mime not in app.config['allowed_mime']:
+            os.remove(temp_file)
+            return "<script> alert('File type not allowed!'); \
+                window.location.replace('');</script>", 415
 
-		file_hash = hashlib.md5(open(temp_file).read()).hexdigest()
-		if file_hash in list_hash():
-			os.remove(temp_file)
-			return "<script> alert('File already exists!'); \
-				window.location.replace('');</script>"
+        file_hash = hashlib.md5(open(temp_file,'rb').read()).hexdigest()
 
-		#everything seems to be alright, saving file
+        if file_hash in list_hash():
+            os.remove(temp_file)
+            return "<script> alert('File already exists!'); \
+                window.location.replace('');</script>"
 
-		title = None
-		description = None
-		ip = None
+        #everything seems to be alright, saving file
 
-		if not request.form["title"]:
-			title = file.filename
-		else:
-			title = request.form["title"]
+        title = None
+        description = None
+        ip = None
 
-		if not request.form["description"]:
-			description = parser.get("application","default_description")
-		else:
-			description = request.form["description"]
+        if not request.form["title"]:
+            title = file.filename
+        else:
+            title = request.form["title"]
 
-		#nginx IP fuckery
+        if not request.form["description"]:
+            description = parser.get("application","default_description")
+        else:
+            description = request.form["description"]
 
-		if parser.getboolean("application", "dev"):
-			ip = request.remote_addr
-		else:
-			ip = request.headers.getlist("X-Forwarded-For")[0]
+        #nginx IP fuckery
 
-		new_video(title, file.filename, ip, file_hash, description)
+        if parser.getboolean("application", "dev"):
+            ip = request.remote_addr
+        else:
+            ip = request.headers.getlist("X-Forwarded-For")[0]
 
-		return redirect('/')
-	
-	else:
-		return render_template('videos.html', videos=Video.query.all())
+
+        print("new video filename:", file.filename )
+        new_video(title, file.filename, ip, file_hash, description)
+
+        return redirect('/')
+    
+    else:
+        return render_template('videos.html', videos=Video.query.all())
 
 
 @app.route('/video/<int:id>')
 def show(id):
-	video = Video.query.get_or_404(id)
-	return render_template('video.html', video=video)
+    video = Video.query.get_or_404(id)
+    return render_template('video.html', video=video)
 
 
 @app.route('/tos')
 def tos():
-	return render_template('tos.html', admin_email = parser.get('admin', 'admin_email'))
+    return render_template('tos.html', admin_email = parser.get('admin', 'admin_email'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def admin_login():
-	if request.method == 'POST':
-		if request.form['user'] == parser.get('admin', 'username') \
-			and request.form['password'] == parser.get('admin','password'):
-			session['logged'] = True
-			return redirect('/admin')
-		else:
-			abort(403)
-	else:
+    if request.method == 'POST':
+        if request.form['user'] == parser.get('admin', 'username') \
+            and request.form['password'] == parser.get('admin','password'):
+            session['logged'] = True
+            return redirect('/admin')
+        else:
+            abort(403)
+    else:
 
-		return render_template('login.html')
+        return render_template('login.html')
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin_page():
 
-	try:
-		logged = session['logged']
-	except KeyError:
-		abort(403)
+    try:
+        logged = session['logged']
+    except KeyError:
+        abort(403)
 
-	if request.method == 'POST':
-		return redirect('/admin')
-	else:
-		return render_template('admin.html', logged=logged, videos=Video.query.all())
+    if request.method == 'POST':
+        return redirect('/admin')
+    else:
+        return render_template('admin.html', logged=logged, videos=Video.query.all())
 
 
 @app.route('/logout')
 def admin_logout():
-	try:
-		if session['logged']:
-			session.pop('logged', None)
-			return redirect('/')
-	except KeyError:
-		return "Not logged"
-		
+    try:
+        if session['logged']:
+            session.pop('logged', None)
+            return redirect('/')
+    except KeyError:
+        return "Not logged"
+        
 
 @app.route('/delete')
 def delete_vid():
-	try:
-		logged = session['logged']
-	except KeyError:
-		abort(403)
+    try:
+        logged = session['logged']
+    except KeyError:
+        abort(403)
 
-	try:
-		id = request.args['v']
-		
-		vid = Video.query.get(id)
-		
-		db.session.delete(vid)
+    try:
+        id = request.args['v']
+        
+        vid = Video.query.get(id)
+        
+        db.session.delete(vid)
 
-		os.remove(os.path.join(app.config['upload_dir'], vid.file_name))
-		os.remove(os.path.join(app.config['thumbs_dir'], vid.file_name + '.jpg'))
+        os.remove(os.path.join(app.config['upload_dir'], vid.file_name))
+        os.remove(os.path.join(app.config['thumbs_dir'], vid.file_name + '.jpg'))
 
-		db.session.commit()
-	except Exception, e:
-		logging.error(e)
-		abort(500)
+        db.session.commit()
+    except Exception as e:
+        logging.error(e)
+        abort(500)
 
-	return redirect('/admin')
+    return redirect('/admin')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'),404
+    return render_template('404.html'),404
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html', admin_email=parser.get("admin", "admin_email")),500
+#@app.errorhandler(500)
+#def internal_server_error(e):
+#    return render_template('500.html', admin_email=parser.get("admin", "admin_email")),500
 
 
 @app.errorhandler(413)
 def request_entity_too_large(e):
-	logging.warning(request.remote_addr + " atempted to upload file larger than allowed")
-	return '''<script> alert("I-it won't f-fit in! - File too large");
-			window.location.replace("");</script>''',413
+    logging.warning(request.remote_addr + " atempted to upload file larger than allowed")
+    return '''<script> alert("I-it won't f-fit in! - File too large");
+            window.location.replace("");</script>''',413
 
 
 #misc funcs
 def new_video(title, file_name, uploader_ip, file_hash, description):
-	#this methods assumes the video is in the temp folder
-	try:
+    #this methods assumes the video is in the temp folder
+    temp_file = os.path.join(tempfile.gettempdir(), file_name)
 
-		temp_file = os.path.join(tempfile.gettempdir(), file_name)
+    container = av.open(temp_file)
+    stream = container.streams.video[0]
+    middle_frame = stream.frames // 2
+    frame =  next((x for i,x in enumerate(container.decode(stream)) if i==middle_frame), None)
+    
+    thumb_filename =  file_name + ".jpg"
+    thumb_path = os.path.join(APP_ROOT, app.config["thumbs_dir"], thumb_filename)
+    frame.to_image().save(thumb_path)
+    
+    def_file = os.path.join(APP_ROOT, app.config["upload_dir"], file_name)
+    shutil.move(temp_file, def_file)
 
-		vs = VideoStream(temp_file)
-		thumb = vs.get_frame_at_sec(int(vs.duration/2)).image()
-		thumb.save(os.path.join(app.config["thumbs_dir"], file_name) + '.jpg')
+    video = Video(title, file_name, file_hash, uploader_ip, description)
 
-		def_file = os.path.join(app.config["upload_dir"] + file_name)
-
-		shutil.move(temp_file, def_file)
-
-		video = Video(title, file_name, file_hash, uploader_ip, description)
-
-		db.session.add(video)
-		db.session.commit()
-
-	except Exception, e:
-		logging.error(e)
-		abort(500)
-
+    db.session.add(video)
+    db.session.commit()
 
 def list_hash():
-	try:
-		hashes = []
-
-		for v in Video.query.all():
-			hashes.append(v.file_md5_hash)
-
-		return hashes
-
-	except Exception as e:
-		logging.error(e)
-		abort(500)
-
+    hashes = []
+    for v in Video.query.all():
+        hashes.append(v.file_md5_hash)
+    return hashes
 
 class Video(db.Model):
 
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(80))
-	file_name = db.Column(db.String(80))
-	file_md5_hash = db.Column(db.String(80))
-	uploader_ip = db.Column(db.String(16))
-	description = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    file_name = db.Column(db.String(80))
+    file_md5_hash = db.Column(db.String(80))
+    uploader_ip = db.Column(db.String(16))
+    description = db.Column(db.String(255))
 
-	def __init__(self, title, file_name, file_md5_hash, uploader_ip, description):
-		self.title = title
-		self.file_name = file_name
-		self.file_md5_hash = file_md5_hash
-		self.uploader_ip = uploader_ip
-		self.description = description
+    def __init__(self, title, file_name, file_md5_hash, uploader_ip, description):
+        self.title = title
+        self.file_name = file_name
+        self.file_md5_hash = file_md5_hash
+        self.uploader_ip = uploader_ip
+        self.description = description
 
-	def __repr__(self):
-		return "<Video %s>" % self.file_name
+    def __repr__(self):
+        return "<Video %s>" % self.file_name
 
 if __name__ == '__main__':
-
     app.run(debug=parser.getboolean("application", "dev"))
     
